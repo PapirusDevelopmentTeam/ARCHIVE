7d21cf87825aa7d18d5628b1e45ce25e9e9981c6
Alexey Varfolomeev <varlesh@gmail.com>
2016-11-10 23:30:08 +0300

    oops

--- install-papirus4.sh
+++ install-papirus4.sh
@@ -2,15 +2,15 @@
 echo "Papirus Aurorae Plasma theme for KDE 4"
 sleep 5
 echo "Create needed folders"
-mkdir -p ~/.kde4/apps/aurorae/themes
+mkdir -p ~/.kde/share/apps/aurorae/themes
 echo "Delete old Papirus Plasma theme ..."
-rm -rf ~/.kde4/apps/aurorae/themes/Papiru*
+rm -rf ~/.kde/share/apps/aurorae/themes/Papiru*
 echo "Download new version from GitHub ..."
 wget -c https://github.com/PapirusDevelopmentTeam/papirus-aurorae-theme/archive/master.zip -O /tmp/papirus-aurorae-theme.zip
 echo "Unpack archive ..."
 unzip -oq /tmp/papirus-aurorae-theme.zip -d /tmp/
 echo "Installing ..."
-cp -R /tmp/papirus-aurorae-theme-master/Papiru* ~/.kde4/apps/aurorae/themes/
+cp -R /tmp/papirus-aurorae-theme-master/Papiru* ~/.kde/share/apps/aurorae/themes/
 echo "Delete cache ..."
 rm -rf /tmp/papiru*
 rm install-papirus4.sh

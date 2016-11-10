fabd1dc47770b7ad0956771f5b142274b2a17e95
Alexey Varfolomeev <varlesh@gmail.com>
2016-11-10 23:36:01 +0300

    fix kde4 path

--- install-papirus4.sh
+++ install-papirus4.sh
@@ -2,15 +2,15 @@
 echo "Papirus Yakuake theme for KDE 4"
 sleep 5
 echo "Create needed folders"
-mkdir -p ~/.kde4/apps/yakuake/skins
+mkdir -p ~/.kde/share/apps/yakuake/skins
 echo "Delete old Papirus Yakuake theme ..."
-rm -rf ~/.kde4/apps/yakuake/skins/papirus
+rm -rf ~/.kde/share/apps/yakuake/skins/papirus
 echo "Download new version from GitHub ..."
 wget -c https://github.com/PapirusDevelopmentTeam/papirus-yakuake-theme/archive/master.zip -O /tmp/papirus-yakuake-theme.zip
 echo "Unpack archive ..."
 unzip -oq /tmp/papirus-yakuake-theme.zip -d /tmp/
 echo "Installing ..."
-cp -R /tmp/papirus-yakuake-theme-master/papirus ~/.kde4/apps/yakuake/skins/
+cp -R /tmp/papirus-yakuake-theme-master/papirus ~/.kde/share/apps/yakuake/skins/
 echo "Delete cache ..."
 rm -rf /tmp/papiru*
 rm install-papirus4.sh

09143644842d3f2b7eb6b5abb786bab651058d0f
Alexey Varfolomeev <varlesh@gmail.com>
2016-11-10 23:34:42 +0300

    fix kde4 path

--- install-papirus4.sh
+++ install-papirus4.sh
@@ -2,15 +2,15 @@
 echo "Papirus Plasma theme for KDE 4"
 sleep 5
 echo "Create needed folders"
-mkdir -p ~/.kde4/apps/desktoptheme
+mkdir -p ~/.kde/share/apps/desktoptheme
 echo "Delete old Papirus Plasma theme ..."
-rm -rf ~/.kde4/apps/desktoptheme/papirus
+rm -rf ~/.kde/share/apps/desktoptheme/papirus
 echo "Download new version from GitHub ..."
 wget -c https://github.com/PapirusDevelopmentTeam/papirus-plasma-theme/archive/master.zip -O /tmp/papirus-plasma-theme.zip
 echo "Unpack archive ..."
 unzip -oq /tmp/papirus-plasma-theme.zip -d /tmp/
 echo "Installing ..."
-cp -R /tmp/papirus-plasma-theme-master/papirus ~/.kde4/apps/desktoptheme/
+cp -R /tmp/papirus-plasma-theme-master/papirus ~/.kde/share/apps/desktoptheme/
 echo "Delete cache ..."
 rm -rf /tmp/papiru*
 rm install-papirus4.sh

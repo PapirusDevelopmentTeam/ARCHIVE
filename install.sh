75b959679225a1b843538d0de72d78ccbcb4f8e0
Alexey Varfolomeev <varlesh@gmail.com>
2016-11-10 15:57:33 +0300

    install script

---
+++ install.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+echo "Papirus icon theme for KDE"
+sleep 5
+echo "Delete old Papirus icon theme ..."
+sudo rm -rf /usr/share/icons/papiru*
+echo "Download new version from GitHub ..."
+wget -c https://github.com/PapirusDevelopmentTeam/papirus-icon-theme-kde/archive/master.zip -O /tmp/papirus-icon-theme-kde.zip
+echo "Unpack archive ..."
+unzip -oq /tmp/papirus-icon-theme-kde.zip -d /tmp/
+echo "Installing ..."
+sudo cp -R /tmp/papirus-icon-theme-kde-master/papiru* /usr/share/icons/
+sudo chmod -R 755 /usr/share/icons/papiru*
+echo "Delete cache ..."
+rm -rf ~/.cache/{plasm*,ico*}
+sudo rm -rf /tmp/papiru*
+echo "Done!"

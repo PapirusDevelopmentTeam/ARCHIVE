3e82bd9124aa4dc4583b54361e9c9da59fdb0b60
Alexey Varfolomeev <varlesh@gmail.com>
2016-11-10 23:30:29 +0300

    fix kde4 path

--- install-papirus4.sh
+++ install-papirus4.sh
@@ -2,11 +2,11 @@
 echo "Papirus color-scheme for KDE 4"
 sleep 5
 echo "Create needed folders"
-mkdir -p ~/.kde4/apps/color-schemes
+mkdir -p ~/.kde/share/apps/color-schemes
 echo "Delete old Papirus color-scheme ..."
-rm -rf ~/.kde4/apps/color-schemes/Papirus.colors
+rm -rf ~/.kde/share/apps/color-schemes/Papirus.colors
 echo "Download new version from GitHub ..."
-wget -c https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-color-scheme/master/Papirus.colors -O ~/.kde4/apps/color-schemes/Papirus.colors
+wget -c https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-color-scheme/master/Papirus.colors -O ~/.kde/share/apps/color-schemes/Papirus.colors
 echo "Delete cache ..."
 rm install-papirus4.sh
 echo "Done!"

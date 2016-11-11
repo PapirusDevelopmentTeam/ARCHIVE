38a37c731a67b5f498f627c6ead522361dbe3687
Alexey Varfolomeev <varlesh@gmail.com>
2016-11-11 12:16:44 +0300

    fix install script

--- install-papirus.sh
+++ install-papirus.sh
@@ -2,15 +2,13 @@
 echo "Papirus wallpapers"
 sleep 5
 echo "Delete old wallpapers ..."
-sudo rm -rf /usr/share/wallpapers/Papiru*
+rm -rf ~/.local/share/wallpapers/Papiru*
 echo "Download new version from GitHub ..."
 wget -c https://github.com/PapirusDevelopmentTeam/papirus-wallpapers/archive/master.zip -O /tmp/papirus-wallpapers.zip
 echo "Unpack archive ..."
 unzip -oq /tmp/papirus-wallpapers.zip -d /tmp/
 echo "Installing ..."
-sudo cp -R /tmp/papirus-wallpapers-master/Papiru* /usr/share/wallpapers/
-sudo chmod -R 755 /usr/share/wallpapers/Papiru*
+cp -R /tmp/papirus-wallpapers-master/Papiru* ~/.local/share/wallpapers/
 echo "Delete cache ..."
 rm -rf /tmp/papiru*
-rm install-papirus.sh
 echo "Done!"

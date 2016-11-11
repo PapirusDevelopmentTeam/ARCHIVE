93616631306a5a7e1e28c30eda3af431e51939d1
Alexey Varfolomeev <varlesh@gmail.com>
2016-11-11 16:12:22 +0300

    fix scripts

--- install-papirus4.sh
+++ install-papirus4.sh
@@ -4,13 +4,13 @@ sleep 5
 echo "Create needed folders"
 mkdir -p ~/.kde/share/apps/aurorae/themes
 echo "Delete old Papirus Plasma theme ..."
-rm -rf ~/.kde/share/apps/aurorae/themes/Papiru*
+rm -rf ~/.kde/share/apps/aurorae/themes/{Papirus,Papirus-Transparent}
 echo "Download new version from GitHub ..."
 wget -c https://github.com/PapirusDevelopmentTeam/papirus-aurorae-theme/archive/master.zip -O /tmp/papirus-aurorae-theme.zip
 echo "Unpack archive ..."
 unzip -oq /tmp/papirus-aurorae-theme.zip -d /tmp/
 echo "Installing ..."
-cp -R /tmp/papirus-aurorae-theme-master/Papiru* ~/.kde/share/apps/aurorae/themes/
+cp -R /tmp/papirus-aurorae-theme-master/{Papirus,Papirus-Transparent} ~/.kde/share/apps/aurorae/themes/
 echo "Delete cache ..."
 rm -rf /tmp/papiru*
 echo "Done!"
--- install-papirus5.sh
+++ install-papirus5.sh
@@ -4,13 +4,13 @@ sleep 5
 echo "Create needed folders"
 mkdir -p ~/.local/share/aurorae/themes
 echo "Delete old Papirus Plasma theme ..."
-rm -rf ~/.local/share/aurorae/themes/Papiru*
+rm -rf ~/.local/share/aurorae/themes/{Papirus,Papirus-Transparent}
 echo "Download new version from GitHub ..."
 wget -c https://github.com/PapirusDevelopmentTeam/papirus-aurorae-theme/archive/master.zip -O /tmp/papirus-aurorae-theme.zip
 echo "Unpack archive ..."
 unzip -oq /tmp/papirus-aurorae-theme.zip -d /tmp/
 echo "Installing ..."
-cp -R /tmp/papirus-aurorae-theme-master/Papiru* ~/.local/share/aurorae/themes/
+cp -R /tmp/papirus-aurorae-theme-master/{Papirus,Papirus-Transparent} ~/.local/share/aurorae/themes/
 echo "Delete cache ..."
 rm -rf ~/.cache/{plasm*,ico*}
 rm -rf /tmp/papiru*

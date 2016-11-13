4c3e5b9530fcbef4accac857117eb4f8acc3e3cd
Alexey Varfolomeev <varlesh@gmail.com>
2016-11-13 22:10:57 +0300

    fix scripts

--- remove-papirus.sh
+++ remove-papirus.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/usr/bin/env bash
 echo "Remove Papirus Aurorae theme"
 rm -rf ~/.kde/share/apps/aurorae/themes/{Papirus,Papirus-Transparent}
 rm -rf ~/.local/share/aurorae/themes/{Papirus,Papirus-Transparent}

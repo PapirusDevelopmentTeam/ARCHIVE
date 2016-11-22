9d7bf0d3edeb49ee1ad67a60db9938931d8a4ac6
varlesh <varlesh@gmail.com>
2016-11-22 09:12:58 +0300

    update script

--- install-papirus-home.sh
+++ install-papirus-home.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 echo "Papirus icon theme for KDE"
-sleep 5
+! which 7za > /dev/null 2>&1 && { echo "Please install p7zip"; exit 1; }
 echo "Delete old Papirus icon theme ..."
 rm -rf ~/.local/share/icons/{papirus,papirus-dark,papirus-arc,papirus-arc-dark}
 echo "Download new version from GitHub ..."
-wget -c https://github.com/PapirusDevelopmentTeam/papirus-icon-theme-kde/archive/master.zip -O /tmp/papirus-icon-theme-kde.zip
+wget -c https://github.com/PapirusDevelopmentTeam/papirus-icon-theme-kde/archive/master.zip \
+    -O /tmp/papirus-icon-theme-kde.zip
 echo "Unpack archive ..."
 7za x /tmp/papirus-icon-theme-kde.zip -o/tmp/
 echo "Installing ..."

28ab62ce69a98e4d8476725df46218db690148a0
varlesh <varlesh@gmail.com>
2016-11-22 10:51:29 +0300

    little fix scripts

--- install-papirus-home.sh
+++ install-papirus-home.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 echo "Papirus icon theme for KDE"
-! which 7za > /dev/null 2>&1 && { echo "Please install p7zip"; exit 1; }
+! which 7za > /dev/null 2>&1 && { echo "Please install p7zip-full"; exit 1; }
 echo "Delete old Papirus icon theme ..."
 rm -rf ~/.local/share/icons/{papirus,papirus-dark,papirus-arc,papirus-arc-dark}
 echo "Download new version from GitHub ..."
 wget -c https://github.com/PapirusDevelopmentTeam/papirus-icon-theme-kde/archive/master.zip \
     -O /tmp/papirus-icon-theme-kde.zip
 echo "Unpack archive ..."
-7za x /tmp/papirus-icon-theme-kde.zip -o/tmp/
+7za x /tmp/papirus-icon-theme-kde.zip -o/tmp/ > /dev/null
 echo "Installing ..."
 mkdir -p ~/.local/share/icons
 cp -R /tmp/papirus-icon-theme-kde-master/{papirus,papirus-dark,papirus-arc,papirus-arc-dark} ~/.local/share/icons/
--- install-papirus-root.sh
+++ install-papirus-root.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 echo "Papirus icon theme for KDE"
-! which 7za > /dev/null 2>&1 && { echo "Please install p7zip"; exit 1; }
+! which 7za > /dev/null 2>&1 && { echo "Please install p7zip-full"; exit 1; }
 echo "Delete old Papirus icon theme ..."
 sudo rm -rf /usr/share/icons/{papirus,papirus-dark,papirus-arc,papirus-arc-dark}
 echo "Download new version from GitHub ..."
 wget -c https://github.com/PapirusDevelopmentTeam/papirus-icon-theme-kde/archive/master.zip \
     -O /tmp/papirus-icon-theme-kde.zip
 echo "Unpack archive ..."
-7za x /tmp/papirus-icon-theme-kde.zip -o/tmp/
+7za x /tmp/papirus-icon-theme-kde.zip -o/tmp/ > /dev/null
 echo "Installing ..."
 sudo cp -R /tmp/papirus-icon-theme-kde-master/{papirus,papirus-dark,papirus-arc,papirus-arc-dark} /usr/share/icons/
 sudo chmod -R 755 /usr/share/icons/{papirus,papirus-dark,papirus-arc,papirus-arc-dark}

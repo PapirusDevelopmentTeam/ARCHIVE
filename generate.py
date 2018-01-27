4c8de9bcaecf9fb965615fa2122a2e1588fe2b63
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2018-01-27 23:45:03 +0100

    add appfilter.xml generation file

--- generate.py
+++ generate.py
@@ -17,3 +17,16 @@ with open(DB_FILE, 'r') as db_obj:
     except ValueError:
         print("Invalid JSON database file")
         exit(0)
+
+app_filter_content = '<?xml version="1.0" encoding="utf-8"?>\n'
+app_filter_content += '<resources>\n'
+
+for icon_src, components in data.items():
+    drawable_name = path.basename(icon_src)
+    for component_info in components:
+        app_filter_content += '\t<item component="ComponentInfo{' + \
+            component_info + '}" drawable="' + drawable_name + '"/>\n'
+
+app_filter_content += '</resources>'
+with open(APPFILTER_FILE, 'w') as app_filter_obj:
+    app_filter_obj.write(app_filter_content)
